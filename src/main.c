f29aae9206a5ffaa4db4f0dc255ba29813cee64d
Ryan Klaproth <ryanklaproth@gmail.com>
2015-11-18 16:34:17 -0600

    Result Screen Logic Implemented
    App now goes between all states and implements the results showing after reaching state 2

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@ Window *window;
 //Set up text layers
 TextLayer *numDiceTextLayer;
 TextLayer *difficultyTextLayer;
-//TextLayer *numOneTextLayer; //Number of dice that rolled 1
+TextLayer *numOneTextLayer; //Number of dice that rolled 1
 //TextLayer *numExplosionsTextLayer; //Number of dice that rolled 10
 //TextLayer *numSuccessTextLayer; //Total Number of Successes
 
@@ -28,14 +28,12 @@ int numDice = 1, difficultyNum = 6;
 //Buffers used to print numbers
 static char diceBuffer[] =       "00000000000";
 static char difficultyBuffer[] = "00000000000";
-static char oneBuffer[] =        "00000000000";
-static char explosionBuffer[] =  "00000000000";
-static char successBuffer[] =    "00000000000";
+char oneBuffer[25];
+//static char explosionBuffer[] =  "00000000000";
+//static char successBuffer[] =    "00000000000";
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context)
 {
-	//text_layer_set_text(textLayer1, "");
-	
 	switch(state)
 	{
 		case 0:
@@ -44,9 +42,18 @@ static void select_click_handler(ClickRecognizerRef recognizer, void *context)
 			break;
 		}
 		case 1:
+		{			
+			state = 2;
+			
+			//numOneTextLayerVisible
+			snprintf(oneBuffer, sizeof(oneBuffer), "Number of 1's: %d", numOne);
+			text_layer_set_text(numOneTextLayer, oneBuffer);
+			
+			break;
+		}
+		case 2:
 		{
 			state = 0;
-			break;
 		}
 	}
 }
@@ -106,25 +113,35 @@ static void main_window_load(Window *window)
 	Layer *windowLayer = window_get_root_layer(window);
 	GRect bounds = layer_get_bounds(windowLayer);
 	
-	//Create the TextLayer with specific bounds
+	//Create identifier text layers
 	numDiceTextLayer = text_layer_create(GRect(0, 25, bounds.size.w - 30, 50));
-	difficultyTextLayer = text_layer_create(GRect(-15, 40, bounds.size.w - 30, 50));
+	difficultyTextLayer = text_layer_create(GRect(-14, 40, bounds.size.w - 30, 50));
+	numOneTextLayer = text_layer_create(GRect(0, 70, bounds.size.w - 30, 50));
+	
+	//Number text layers
 	clickNumDiceLayer = text_layer_create(GRect(100, 25, bounds.size.w - 30, 50));
 	clickDifficultyLayer = text_layer_create(GRect(100, 40, bounds.size.w - 30, 50));
 	
-	//textLayer1
+	//numDiceTextLayer
 	text_layer_set_background_color(numDiceTextLayer, GColorClear);
 	text_layer_set_text_color(numDiceTextLayer, GColorBlack);
 	text_layer_set_text(numDiceTextLayer, "Number of Dice: ");
 	text_layer_set_font(numDiceTextLayer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
 	text_layer_set_text_alignment(numDiceTextLayer, GTextAlignmentCenter);
 	
-	//textLayer2
+	//difficultyTextLayer
 	text_layer_set_background_color(difficultyTextLayer, GColorClear);
 	text_layer_set_text_color(difficultyTextLayer, GColorBlack);
 	text_layer_set_text(difficultyTextLayer, "Difficulty: ");
 	text_layer_set_font(difficultyTextLayer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
-	text_layer_set_text_alignment(difficultyTextLayer, GTextAlignmentCenter);
+	text_layer_set_text_alignment(difficultyTextLayer, GTextAlignmentCenter);		
+		
+	//numOneTextLayer
+	text_layer_set_background_color(numOneTextLayer, GColorClear);
+	text_layer_set_text_color(numOneTextLayer, GColorBlack);
+	text_layer_set_text(numOneTextLayer, "");
+	text_layer_set_font(numOneTextLayer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+	text_layer_set_text_alignment(numOneTextLayer, GTextAlignmentCenter);
 	
 	//clickNumLayer
 	text_layer_set_text_color(clickNumDiceLayer, GColorBlack);
@@ -143,6 +160,7 @@ static void main_window_load(Window *window)
 	layer_add_child(windowLayer, text_layer_get_layer(difficultyTextLayer));
 	layer_add_child(windowLayer, text_layer_get_layer(clickNumDiceLayer));
 	layer_add_child(windowLayer, text_layer_get_layer(clickDifficultyLayer));
+	layer_add_child(windowLayer, text_layer_get_layer(numOneTextLayer));
 }
 
 static void main_window_unload(Window *window) 
@@ -152,6 +170,7 @@ static void main_window_unload(Window *window)
 	text_layer_destroy(difficultyTextLayer);
 	text_layer_destroy(clickNumDiceLayer);
 	text_layer_destroy(clickDifficultyLayer);
+	text_layer_destroy(numOneTextLayer);
 }
 
 static void init() 
